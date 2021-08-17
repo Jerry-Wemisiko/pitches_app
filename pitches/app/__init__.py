5b7d69730604cc8633fc0593f9a221f9f6945e30
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-08-17 23:42:09 +0300

    create app configs and initialized extensions

--- pitches/app/__init__.py
+++ pitches/app/__init__.py
@@ -1,4 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask import Flask
+from config import config_options
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+
+
--- pitches/config.py
+++ pitches/config.py
@@ -3,3 +3,15 @@ import os
 class Config:
 
      SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jerry@localhost/pitches'
+
+class ProdConfig:
+      
+      pass
+
+class DevConfig:  
+      SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jerry@localhost/pitches'
+   
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
