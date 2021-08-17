29bfdea013e7914128f00a3d6d38a3d8d136aa12
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-08-17 23:30:59 +0300

    installed dependecies

---
+++ pitches/app/models.py
@@ -0,0 +1,12 @@
+from . import db
+
+class User(db.Model):
+    __table__name = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True)
+    about = db.Column(db.String(255))
+    
+
+    def __repr__(self):
+        return f'User {self.username}'
---
+++ pitches/config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config:
+
+     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jerry@localhost/pitches'
