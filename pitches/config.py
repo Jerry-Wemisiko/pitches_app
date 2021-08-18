f1ad9f9b5b88d87526dd9c98df086c26cc6b85c1
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-08-18 17:02:22 +0300

    added email configuration and config_options object inside config.py

--- pitches/config.py
+++ pitches/config.py
@@ -1,4 +1,31 @@
 import os
+from re import DEBUG
+
+from flask import config
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:skyles@localhost/pitches'
+
+    #mail configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://moringa:skyles@localhost/pitches_test'
+    
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:skyles@localhost/pitches'
+    DEBUG = True
+
+config_option= {
+    'development': DevConfig,
+    'prodection':ProdConfig,
+    'test':TestConfig
+}
