7e651578dd1720335f2faae7b223927e6f02d521
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-08-19 14:01:59 +0300

    user authentication

--- pitches/app/main/views.py
+++ pitches/app/main/views.py
@@ -1,14 +1,25 @@
 from flask import render_template
 from . import main
-from ..models import User
+from flask_login import login_required
+from ..models import Pitch, User
 from .. import  db
 
 #Views
-@main.route
+@main.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
     '''
     title = 'Welcome to Pitches'
+    pitches = Pitch.query.all()
+    pickuplines = Pitch.query.filter_by(category = 'pickuplines').all()
+    interviews = Pitch.query.filter_by(category = 'interviews').all()
+    product = Pitch.query.filter_by(category = 'product').all()
 
-    return render_template('index.html',title = title)
+
+
+    return render_template('index.html',title = title,piches=pitches,interviews=interviews,pickuplines=pickuplines,product=product)
+
+# @main.route('/add_pich')
+# @login_required
+# def new_pitch(id):
